eecc74340920fb11fe83ea19138100a12da5513b
Tommy Kvant <tommy.kvant@gmail.com>
2022-12-04 22:03:11 +0100

    Day04 Part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -10,23 +10,20 @@ fun main() {
                 .let { it[0] to it[1] }
         }
 
-    fun part1(input: List<String>): Int {
-        val groups = parseInput(input)
+    fun part1(input: List<String>) = parseInput(input)
+        .count { elves ->
+            val combined = elves.first.intersect(elves.second)
+            combined.size == elves.first.size || combined.size == elves.second.size
+        }
 
-        return groups
-            .count { elves ->
-                val combined = elves.first.intersect(elves.second)
-                combined.size == elves.first.size || combined.size == elves.second.size
-            }
-    }
-
-    fun part2() {
-
-    }
+    fun part2(input: List<String>) = parseInput(input)
+        .count { elves -> elves.first.intersect(elves.second).isNotEmpty() }
 
     val testInput = readInput("Day04_test")
     println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day04")
     println(part1(input))
+    println(part2(input))
 }
