96982c1baba39a489d22c40443af69823f783110
Tommy Kvant <tommy.kvant@gmail.com>
2022-12-04 13:06:42 +0100

    Day01 Part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .fold(mutableListOf<MutableList<Int>>(mutableListOf())) { list, line ->
+                if ("" == line) {
+                    list.add(mutableListOf())
+                } else {
+                    list.last().add(line.toInt())
+                }
+                list
+            }
+            .maxOf(MutableList<Int>::sum)
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +18,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    check(part1(testInput) == 1)
+    println(part1(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
