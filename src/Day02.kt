c031aa8f977fe5a876d4ace77ea759cd45ebdba1
Tommy Kvant <tommy.kvant@gmail.com>
2022-12-04 16:55:03 +0100

    Day02 Part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -5,25 +5,26 @@ fun main() {
         SHAPE.SCISSORS -> 3
     }
 
-    fun roundScore(opponentShape: SHAPE, yourShape: SHAPE) = when {
-        (opponentShape == SHAPE.ROCK && yourShape == SHAPE.PAPER) ||
-                (opponentShape == SHAPE.PAPER && yourShape == SHAPE.SCISSORS) ||
-                (opponentShape == SHAPE.SCISSORS && yourShape == SHAPE.ROCK) -> 6
+    fun beatingShape(shape: SHAPE) = when (shape) {
+        SHAPE.ROCK -> SHAPE.PAPER
+        SHAPE.PAPER -> SHAPE.SCISSORS
+        SHAPE.SCISSORS -> SHAPE.ROCK
+    }
 
+    fun losingShape(shape: SHAPE) = when (shape) {
+        SHAPE.ROCK -> SHAPE.SCISSORS
+        SHAPE.PAPER -> SHAPE.ROCK
+        SHAPE.SCISSORS -> SHAPE.PAPER
+    }
+
+    fun roundScore(opponentShape: SHAPE, yourShape: SHAPE) = when {
+        yourShape == beatingShape(opponentShape) -> 6
         opponentShape.ordinal == yourShape.ordinal -> 3
         else -> 0
     }
 
-    fun part1(strategy: List<Pair<SHAPE, SHAPE>>) = strategy.sumOf { (opponentShape, yourShape) ->
-        roundScore(opponentShape, yourShape) + shapeScore(yourShape)
-    }
-
-    fun part2(strategy: List<Pair<SHAPE, SHAPE>>) {
-
-    }
-
-    fun parseStrategy(input: List<String>): List<Pair<SHAPE, SHAPE>> {
-        return input.map { line ->
+    fun part1(input: List<String>): Int {
+        val strategy = input.map { line ->
             val opponentShape = when (val char = line[0]) {
                 'A' -> SHAPE.ROCK
                 'B' -> SHAPE.PAPER
@@ -39,19 +40,57 @@ fun main() {
 
             opponentShape to yourShape
         }
+        return strategy.sumOf { (opponentShape, yourShape) ->
+            roundScore(opponentShape, yourShape) + shapeScore(yourShape)
+        }
     }
 
+    fun part2(input: List<String>): Int {
+        val strategy = input.map { line ->
+            val opponentShape = when (val char = line[0]) {
+                'A' -> SHAPE.ROCK
+                'B' -> SHAPE.PAPER
+                'C' -> SHAPE.SCISSORS
+                else -> throw IllegalArgumentException("Unknown input '$char'")
+            }
+            val outcome = when (val char = line[2]) {
+                'X' -> OUTCOME.LOSE
+                'Y' -> OUTCOME.DRAW
+                'Z' -> OUTCOME.WIN
+                else -> throw IllegalArgumentException("Unknown input '$char'")
+            }
+
+            opponentShape to outcome
+        }
+
+        return strategy.sumOf { (opponentShape, outcome) ->
+            val yourShape = when (outcome) {
+                OUTCOME.LOSE -> losingShape(opponentShape)
+                OUTCOME.DRAW -> opponentShape
+                OUTCOME.WIN -> beatingShape(opponentShape)
+            }
+            roundScore(opponentShape, yourShape) + shapeScore(yourShape)
+        }
+    }
+
+
     val testInput = readInput("Day02_test")
-    val testStrategy = parseStrategy(testInput)
-    println(part1(testStrategy))
+    println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day02")
-    val strategy = parseStrategy(input)
-    println(part1(strategy))
+    println(part1(input))
+    println(part2(input))
 }
 
 enum class SHAPE {
     ROCK,
     PAPER,
     SCISSORS
+}
+
+enum class OUTCOME {
+    LOSE,
+    DRAW,
+    WIN
 }
