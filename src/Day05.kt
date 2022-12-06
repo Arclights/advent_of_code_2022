bf7532cc5906cc3ca229c71daaf7240d894f374c
Tommy Kvant <tommy.kvant@gmail.com>
2022-12-06 22:12:53 +0100

    Day05 Part1

---
+++ src/Day05.kt
@@ -0,0 +1,48 @@
+fun main() {
+    fun part1(input: Input): String = input
+        .instructions
+        .fold(input.stacks) { stacks, (move, from, to) ->
+            stacks[to - 1] = stacks[to - 1].plus(stacks[from - 1].takeLast(move).reversed())
+            stacks[from - 1] = stacks[from - 1].dropLast(move)
+            stacks
+        }
+        .joinToString("") { it.last().toString() }
+
+    fun part2() {
+
+    }
+
+    fun parseInput(rawInput: List<String>): Input {
+        fun parseStacks(rawStacks: List<String>): MutableList<String> {
+            return rawStacks
+                .map { line -> line.drop(1).windowed(1, 4) }
+                .reversed()
+                .reduce { acc, list ->
+                    val paddedList = list.plus(List(acc.size - list.size) { " " })
+                    acc.zip(paddedList) { a, b -> a.plus(b) }
+                }
+                .map { it.drop(1) }
+                .map { it.filter { item -> item != ' ' } }
+                .toMutableList()
+        }
+
+        fun parseInstructions(rawInstructions: List<String>): List<Triple<Int, Int, Int>> {
+            return rawInstructions
+                .map { it.split(" ") }
+                .map { Triple(it[1].toInt(), it[3].toInt(), it[5].toInt()) }
+        }
+
+        return Input(
+            parseStacks(rawInput.takeWhile { it != "" }),
+            parseInstructions(rawInput.dropWhile { it != "" }.drop(1))
+        )
+    }
+
+    val testInput = parseInput(readInput("Day05_test"))
+    println(part1(testInput))
+
+    val input = parseInput(readInput("Day05"))
+    println(part1(input))
+}
+
+data class Input(val stacks: MutableList<String>, val instructions: List<Triple<Int, Int, Int>>)
