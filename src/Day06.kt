ba159543a1063d2210246805b77bab01a0117a1f
Tommy Kvant <tommy.kvant@gmail.com>
2022-12-06 21:13:42 +0100

    Day06 Part2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,22 +1,19 @@
 fun main() {
-    fun part1(buffer: String): Int = buffer
-        .windowed(4)
-        .mapIndexed { i, chars ->
-            if (chars.toSet().size == 4) {
-                i + 4
-            } else {
-                -1
-            }
-        }
+
+    fun findFirstXUnique(buffer: String, x: Int): Int = buffer
+        .windowed(x)
+        .mapIndexed { i, chars -> if (chars.toSet().size == x) i + x else -1 }
         .first { it != -1 }
 
-    fun part2() {
+    fun part1(buffer: String): Int = findFirstXUnique(buffer, 4)
 
-    }
+    fun part2(buffer: String): Int = findFirstXUnique(buffer, 14)
 
     val testInput = readInput("Day06_test")
     println(testInput.map(::part1))
+    println(testInput.map(::part2))
 
     val input = readInput("Day06")[0]
     println(part1(input))
+    println(part2(input))
 }
