121a81ba82ba0d921bd1b689241ffa8727bc806e
Tommy Kvant <tommy.kvant@gmail.com>
2022-12-08 23:29:47 +0100

    Day08 Part2

--- src/Day08.kt
+++ src/Day08.kt
@@ -7,21 +7,26 @@ fun main() {
 
     fun <T> List<List<T>>.asMatrixString() = this.joinToString("\n") { row -> row.joinToString("") }
 
-    fun findVisible(trees: List<Tree>) = trees
+    fun findVisible(trees: List<Tree>, comparer: (Int, Int) -> Boolean) = trees
         .fold(listOf<Tree>()) { previousTrees, tree ->
-            if (previousTrees.isEmpty() || tree.height > previousTrees.last().height) {
+            if (previousTrees.isEmpty() || comparer(tree.height, previousTrees.last().height)) {
                 previousTrees.plus(tree)
             } else {
                 previousTrees
             }
         }
 
+    fun findVisibleFromEdge(trees: List<Tree>) = findVisible(trees) { a, b -> a > b }
+
+    fun findVisibleFromTree(trees: List<Tree>, currentTreeHeight: Int) =
+        findVisible(trees) { _, b -> b < currentTreeHeight }
+
     fun findVisible(input: List<List<Tree>>): Set<Tree> {
         return input.indices
             .flatMap { row ->
                 listOf(
-                    findVisible(input[row]),
-                    findVisible(input[row].reversed())
+                    findVisibleFromEdge(input[row]),
+                    findVisibleFromEdge(input[row].reversed())
                 ).flatten()
             }
             .toSet()
@@ -35,6 +40,28 @@ fun main() {
         .toSet()
         .size
 
+    fun scenicScore(input: List<List<Tree>>, row: Int, column: Int): Int {
+        val transposed = input.transpose()
+        val currentTreeHeight = input[row][column].height
+        return listOf(
+            input[row].drop(column + 1),
+            input[row].reversed().drop(input[row].size - column),
+            transposed[column].drop(row + 1),
+            transposed[column].reversed().drop(transposed[column].size - row)
+        )
+            .map { findVisibleFromTree(it, currentTreeHeight) }
+            .map { it.size }
+            .reduce(Int::times)
+    }
+
+    fun part2(input: List<List<Tree>>): Int {
+        return input.indices
+            .flatMap { row ->
+                input.first().indices.map { column -> scenicScore(input, row, column) }
+            }
+            .max()
+    }
+
     fun parseInput(input: List<String>) = input.mapIndexed { row, line ->
         line.mapIndexed { column, treeHeight ->
             Tree(treeHeight.digitToInt(), row, column)
@@ -44,12 +71,18 @@ fun main() {
     val testInput = readInput("Day08_test")
     val parsedTestInput = parseInput(testInput)
     println(part1(parsedTestInput))
+    println(part2(parsedTestInput))
 
     println()
 
     val input = readInput("Day08")
     val parsedInput = parseInput(input)
     println(part1(parsedInput))
+    println(part2(parsedInput))
 }
 
-data class Tree(val height: Int, val row: Int, val column: Int)
+data class Tree(val height: Int, val row: Int, val column: Int) {
+    override fun toString(): String {
+        return "($height, $row, $column)"
+    }
+}
